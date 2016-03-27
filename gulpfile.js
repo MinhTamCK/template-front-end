20059c7af491de871bcdde8cb47e3a13ee7301c3
MinhTam <minhtamck4@gmail.com>
2016-03-28 01:33:25 +0700

    Update source

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,19 @@ var htmlhint = require('gulp-htmlhint');
 var sass = require('gulp-sass');
 var csslint = require('gulp-csslint');
 var jshint = require('gulp-jshint');
+// var concatCss = require('gulp-concat-css');
+var concat = require('gulp-concat');
+var minify = require('gulp-minifier');
+var cache = require('gulp-cache');
 
-gulp.task('default', ['watch']);
+
+gulp.task('default', ['clearCache','watch']);
 
 // task watch
 gulp.task('watch', function() {
-    gulp.watch('./src/**/*.html', ['htmlhint']);
-    gulp.watch('./src/css/**/*.scss', ['build-css']);
-    gulp.watch('./static/css/**/*.css', ['csslint']); // Recommend watch each file
-    gulp.watch('./src/js/**/*js', ['jshint']);
+    gulp.watch('./src/**/*.html', ['htmlhint', 'copyHtml']);
+    gulp.watch('./src/css/**/*.scss', ['build-css', 'csslint', 'concatCss']);
+    gulp.watch('./src/js/**/*js', ['jshint', 'copyjavascript']);
 });
 
 
@@ -38,4 +42,41 @@ gulp.task('jshint', function() {
     return gulp.src('./src/js/**/*js')
         .pipe(jshint('.jshintrc'))
         .pipe(csslint.reporter());
+});
+/* concat css  task */
+gulp.task('concatCss', function() {
+    return gulp.src(['./static/css/libs.css','./static/css/style.css'])
+        .pipe(concat("./css/bundle.css"))
+        .pipe(gulp.dest('out/'));
+});
+/* copy html task */
+gulp.task('copyHtml', function() {
+    return gulp.src('./src/html/*.html')
+        .pipe(gulp.dest('out/html/'));
+});
+/* copy js task */
+gulp.task('copyjavascript', function() {
+    return gulp.src(['./src/js/lib/*.js', './src/js/plugin/*.js', './src/js/app.js'])
+        .pipe(concat('./js/all.js'))
+        .pipe(gulp.dest('out/'));
+});
+/* minify js/css task */
+gulp.task('minify', function() {
+    return gulp.src('./out/**/*')
+        .pipe(minify({
+            minify: true,
+            collapseWhitespace: true,
+            conservativeCollapse: true,
+            minifyJS: true,
+            minifyCSS: true,
+            getKeptComment: function(content, filePath) {
+                var m = content.match(/\/\*![\s\S]*?\*\//img);
+                return m && m.join('\n') + '\n' || '';
+            }
+        }))
+        .pipe(gulp.dest('out/'));
+});
+/* clear all task */
+gulp.task('clearCache', function() {
+    return cache.clearAll();
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,133 +1,17 @@
 /* particlesJS.load(@dom-id, @path-json*/
 var flag = true;
-particlesJS.load('stu-particles', '../javascript/data/particles.json');
-// setInterval(function() {
-//     if (flag) {
-//         particlesJS.load('stu-particles', '../javascript/data/particles-2.json');
-//         flag = false;
-//     } else {
-//         particlesJS.load('stu-particles', '../javascript/data/particles.json');
-//         flag = true;
-//     }
-// }, 10000);
+particlesJS.load('stu-particles', '../js/data/particles.json',hideLoading);
 
+function hideLoading() {
+	$('.loading').addClass('hidden');
+};
 // Trianglify create
 var pattern = Trianglify({
 	width: window.innerWidth,
 	height: 500
 });
+// Backgrout trianglify
 $('.stu-trianglify').append(pattern.canvas());
-// Contact
-// var trianglifyContact = Trianglify({
-//     width: window.innerWidth,
-//     variance: "0",
-//     seed: '4txiq',
-//     x_colors: 'random'
-// });
-// $('.stu-trianglify-contact').append(trianglifyContact.canvas());
+// Declare angularjs
 angular.module('startupbyme', ['ngMessages']);
-àkasf
-// var prev = 0;
-// var $window = $(window);
-// var nav = $('.navbar');
 
-// $window.on('scroll', function(){
-//   var scrollTop = $window.scrollTop();
-//   nav.toggleClass('hidden', scrollTop > prev);
-//   prev = scrollTop;
-// });
-// Plugin jquery
-/**
- *  @name plugin top navigation
- *  @description go to element reference
- *  @version 1.0
- *  @options
- *    option
- *  @events
- *    event
- *  @methods
- *    init
- *    destroy
- */
-(function($) {
-	'use strict';
-
-	var pluginName = 'top-navigation',
-		$htmlBodyTag = $('html body');
-
-	var goToElementReference = function(id) {
-		var $elementReference = $(id);
-		// Check id exist
-		if ($elementReference.length) {
-			$htmlBodyTag.animate({
-				scrollTop: $elementReference.offset().top
-			}, 1000);
-		}
-	};
-
-	var removeClassActive = function(liTagList) {
-		liTagList.each(function() {
-			var $liTag = $(this);
-			if ($liTag.hasClass('active')) {
-				$liTag.removeClass('active');
-				return false;
-			}
-		});
-	};
-
-	function Plugin(element, options) {
-		this.element = $(element);
-		this.options = $.extend({}, $.fn[pluginName].defaults, this.element.data(), options);
-		this.init();
-	}
-
-	Plugin.prototype = {
-		init: function() {
-			var that = this,
-				el = that.element,
-				$aTagList = el.find('li a'),
-				$liTagList = el.find('li');
-
-
-			$aTagList.on('click', function(event) {
-				event.preventDefault();
-				var $current = el.find('.active a');
-				var $aTag = $(this);
-				if ($current.is($aTag)) {
-					return;
-				}
-
-				removeClassActive($liTagList);
-				$aTag.parent('li').addClass('active');
-				var $id = $aTag.attr('href');
-				goToElementReference($id);
-			});
-
-		},
-
-		destroy: function() {
-			// remove events
-			// deinitialize
-			$.removeData(this.element[0], pluginName);
-		}
-	};
-
-	$.fn[pluginName] = function(options, params) {
-		return this.each(function() {
-			var instance = $.data(this, pluginName);
-
-			if (!instance) {
-				$.data(this, pluginName, new Plugin(this, options));
-			} else if (instance[options]) {
-				instance[options](params);
-			}
-		});
-	};
-
-	$.fn[pluginName].defaults = {};
-
-	$(function() {
-		$('[data-' + pluginName + ']')[pluginName]();
-	});
-
-}(jQuery));
