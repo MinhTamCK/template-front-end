6d69b07333ec743fb5daf43f460b86e25421769c
tam.nm <tam.nm@sutrixmedia.com>
2016-04-13 16:06:33 +0700

    Add skills.

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,121 @@
-/* particlesJS.load(@dom-id, @path-json*/
-var flag = true;
-particlesJS.load('stu-particles', '../js/data/particles.json', hideLoading);
-
-function hideLoading() {
-    $('.loading').addClass('hidden');
+'use strict'
+/* ParticlesJS */
+var Slider = {
+    init: function() {
+        particlesJS.load('stu-particles', '../js/data/particles.json', this.hideLoading());
+    },
+    hideLoading: function() {
+        $('.loading').addClass('hidden');
+    }
 };
-// Trianglify create
+
+Slider.init();
+
+/* Backgroud trianglify */
 var pattern = Trianglify({
     width: window.innerWidth,
     height: 500
 });
-// Backgrout trianglify
+
 $('.stu-trianglify').append(pattern.canvas());
-// Declare angularjs
+
+/* Declare angularjs */
 angular.module('startupbyme', ['ngMessages']);
-// Document ready
+
+var DrawRaphael = {
+    init: function() {
+        this.diagram();
+    },
+    random: function(l, u) {
+        return Math.floor((Math.random() * (u - l + 1)) + l);
+    },
+    diagram: function() {
+        var r = Raphael('my-skills', 600, 500),
+            rad = 73,
+            defaultText = 'Skills',
+            speed = 250;
+
+        r.circle(300, 300, 85).attr({
+            stroke: 'none',
+            fill: '#193340'
+        });
+
+        var title = r.text(300, 300, defaultText).attr({
+            font: '20px Arial',
+            fill: '#fff'
+        }).toFront();
+
+        r.customAttributes.arc = function(value, color, rad) {
+            var v = 3.6 * value,
+                alpha = v == 360 ? 359.99 : v,
+                random = DrawRaphael.random(91, 240),
+                a = (random - alpha) * Math.PI / 180,
+                b = random * Math.PI / 180,
+                sx = 300 + rad * Math.cos(b),
+                sy = 300 - rad * Math.sin(b),
+                x = 300 + rad * Math.cos(a),
+                y = 300 - rad * Math.sin(a),
+                path = [
+                    ['M', sx, sy],
+                    ['A', rad, rad, 0, +(alpha > 180), 1, x, y]
+                ];
+            return {
+                path: path,
+                stroke: color
+            }
+        }
+
+        $('.params').find('.param').each(function(i) {
+            var t = $(this),
+                color = t.find('.color').val(),
+                value = t.find('.percent').val(),
+                text = t.find('.text').text();
+
+            rad += 30;
+            var z = r.path().attr({
+                arc: [value, color, rad],
+                'stroke-width': 26
+            });
+
+            z.mouseover(function() {
+                this.animate({
+                    'stroke-width': 50,
+                    opacity: .75
+                }, 1000, 'elastic');
+                if (Raphael.type != 'VML') //solves IE problem
+                    this.toFront();
+                title.stop().animate({
+                    opacity: 0
+                }, speed, '>', function() {
+                    this.attr({
+                        text: text + '\n' + value + '%'
+                    }).animate({
+                        opacity: 1
+                    }, speed, '<');
+                });
+            }).mouseout(function() {
+                this.stop().animate({
+                    'stroke-width': 26,
+                    opacity: 1
+                }, speed * 4, 'elastic');
+                title.stop().animate({
+                    opacity: 0
+                }, speed, '>', function() {
+                    title.attr({
+                        text: defaultText
+                    }).animate({
+                        opacity: 1
+                    }, speed, '<');
+                });
+            });
+        });
+
+    }
+};
+
+/* Document ready */
 $(document).ready(function() {
+    /* Slide bar */
     $('.technologies-image').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
@@ -42,4 +142,7 @@ $(document).ready(function() {
 
         ]
     });
+
+    /* Call drawRaphael */
+    DrawRaphael.init();
 });
